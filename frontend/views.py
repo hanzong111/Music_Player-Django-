61e6dd05afddc52488ac3cc94cf2e199c8fcf21e
Ong Jing Hanz <hanzong111@LAPTOP-OSC5HOD8>
2024-07-28 17:02:10 +0800

    Done setting up react and bable

--- frontend/views.py
+++ frontend/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+# Allows use to render the index.html file (React will take care of the rendering)
+def index(request, *args, **kwargs):
+    return render(request, 'frontend/index.html')
