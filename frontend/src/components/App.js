e50e2e8374baa4865921dd8932aec63c2ca5ad94
Ong Jing Hanz <hanzong111@LAPTOP-OSC5HOD8>
2024-07-29 16:20:25 +0800

    Added new components and able to render all components in a single div element

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,5 +1,8 @@
 import React, {Component} from "react";
 import { render } from "react-dom";
+import HomePage from "./HomePage";
+import RoomJoinPage from "./RoomJoinPage";
+import CreateRoomPage from "./CreateRoomPage";
 
 export default class App extends Component {
     constructor(props){
@@ -7,6 +10,13 @@ export default class App extends Component {
     }
 
     render() {
-        return (<h1>testing react Code</h1>)
+        return (
+        <div>
+            <RoomJoinPage/>
+            <CreateRoomPage/>
+            <HomePage/>
+        </div>);
     }
 }
+
+render(<App />, document.getElementById("app"));
